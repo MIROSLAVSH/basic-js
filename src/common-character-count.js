5ee9f571103ed0b6d457acfaaeb080d618709759
MIROSLAVSH <sharkovskiymiroslav@gmail.com>
2022-04-26 20:27:19 +0300

    docs: wgg

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -13,13 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function getCommonCharacterCount(s1, s2) {
   var count = 0;
-
-    for(let i in s1) {
-        s2.includes(s1[i]) ? count++ : false;
+    while(s1.length && s2.length){
+        if(s2.includes(s1.charAt(0))){
+            count++;
+            s2 = s2.replace(s1.charAt(0),"");
+            s1 = s1.slice(1);
+        }
+        else {
+            s1 = s1.slice(1);
+        }
     }
-
-    return count - 1;
+    return count;
 }
+
+
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 
