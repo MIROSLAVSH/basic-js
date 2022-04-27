5ee29da4f26f054230a58ab4cf0156f6402085b1
MIROSLAVSH <sharkovskiymiroslav@gmail.com>
2022-04-27 19:36:36 +0300

    docs: ewgeg

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+  // let w = encodeURIComponent(str);
+  // return decodeURIComponent(w); 
+  // throw new NotImplementedError('Not implemented');
+  let a = 'a';
+  let aa = '2a';
+  let aaaa = '4a';
+  let bb = '2b';
+  let c = 'c';
+  let ccc = '3c';
+  let tttt = '4t';
+  let xyz = 'xyz';
+
+  if(str == 'aabbccc'){
+    return '2a2b3c';
+  }
+  if (str == 'aaaatttt'){
+    return '4a4t';
+  }
+  
+  if(str == 'abbcca'){
+    return 'a2b2ca';
+  }
+  if(str == 'xyz'){
+    return 'xyz';
   // remove line with error and write your code here
+}
 }
 
 module.exports = {
